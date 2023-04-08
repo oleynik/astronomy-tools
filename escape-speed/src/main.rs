c8c2abf931d070aa509aef4f91306db5f05bc321
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-04-09 00:02:54 +0200

    Improved implementation of validation: created separte function for each argument

--- escape-speed/src/main.rs
+++ escape-speed/src/main.rs
@@ -1,33 +1,49 @@
-use escape_speed::*;
+use escape_speed::{solar_system, HeavenlyBody};
 use std::env;
 
 fn supported_velocities() -> Vec<&'static str> {
     vec!["v1"]
 }
 
-fn validation(args: &Vec<String>) -> Result<(), String> {
-    if args.len() != 4 {
-        return Err(String::from("Unexpected number of arguments."));
+fn v_number_of_arguments(n: usize) -> Result<(), String> {
+    if n != 4 {
+        Err(String::from("Unexpected number of arguments."))
+    } else {
+        Ok(())
     }
-    if !solar_system()
-        .iter()
-        .map(|e| e.name())
-        .any(|n| n == args[1])
-    {
+}
+fn v_objects_name(name: &String) -> Result<(), String> {
+    if !solar_system().iter().map(|e| e.name()).any(|n| n == *name) {
         return Err(String::from("Unknown name of Heavenly Object."));
+    } else {
+        Ok(())
     }
+}
+fn v_velocity(velocity: &String) -> Result<(), String> {
     let velocities = supported_velocities();
-    if !velocities.contains(&args[2].as_str()) {
+    if !velocities.contains(&velocity.as_str()) {
         return Err(format!(
             "Unsupported velocity. Supported velocities are: [{}]",
             velocities.join(" ")
         ));
+    } else {
+        Ok(())
     }
-    match args[3].parse::<f64>() {
+}
+fn v_distance(distance: &String) -> Result<(), String> {
+    match distance.parse::<f64>() {
         Ok(v) if v >= 0_f64 => Ok(()),
         Ok(v) => Err(format!("Distance cannot be negative: {}", v)),
         Err(e) => Err(e.to_string()),
     }
+}
+
+fn validation(args: &Vec<String>) -> Result<(), String> {
+    v_number_of_arguments(args.len())?;
+    v_objects_name(&args[1])?;
+    v_velocity(&args[2])?;
+    v_distance(&args[3])?;
+    Ok(())
 }
 
 fn main() -> Result<(), String> {
